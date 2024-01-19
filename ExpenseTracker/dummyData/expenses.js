8a879f74b1b0253d63fae5e0d82bbfa4d1dbbd1c
Roy Gero <roy.gero.ii@gmail.com>
2024-01-19 12:57:07 -0500

    Adding dummy expense data.

---
+++ ExpenseTracker/dummyData/expenses.js
@@ -0,0 +1,45 @@
+export const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: "New Hockey Stick",
+    amount: 110.0,
+    date: new Date('2024-01-01')
+  },
+  {
+    id: 'e2',
+    description: "Groceries",
+    amount: 55.22,
+    date: new Date('2024-01-15')
+  },
+  {
+    id: 'e3',
+    description: "Gas",
+    amount: 42.03,
+    date: new Date('2024-01-01')
+  },
+  {
+    id: 'e4',
+    description: "Rudy's",
+    amount: 40,
+    date: new Date('2024-01-19')
+  },
+  {
+    id: 'e5',
+    description: "Patreon",
+    amount: 2.22,
+    date: new Date('2024-01-10')
+  },
+  {
+    id: 'e6',
+    description: "Video",
+    amount: 5.99,
+    date: new Date('2024-01-01')
+  },
+  {
+    id: 'e7',
+    description: "Palworld",
+    amount: 23.56,
+    date: new Date('2024-01-18')
+  },
+
+]
